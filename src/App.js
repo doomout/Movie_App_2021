b07a18289fa38a0781203f1460c3f5d56720f785
김현구 <doomout1@gmail.com>
2021-12-11 02:40:07 +0900

    map 사용편

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+//인자를 받는다
+function Food({ name, picture }) {
+  //console.log(fav); //로그찍기
+  //{ fav } = props;
+  return (
+      <div>
+        <h2>I like {name}</h2>
+        <img src={picture} />
+      </div>
+    );
+}
+
+const foodILike = [
+  {
+    name: '김치',
+    image: 'http://08food.com/web/product/big/201801/33_shop1_243196.jpg',
+  },
+  {
+    name: '라면',
+    image: 'https://health.chosun.com/site/data/img_dir/2020/09/07/2020090702900_0.jpg',
+  },
+  {
+    name: '김밥',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Gimbap_%28pixabay%29.jpg/220px-Gimbap_%28pixabay%29.jpg',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => (<Food name={dish.name} picture={dish.image}/>))}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//ID가 root인 엘리먼트에 App 컴포넌트를 그린다.
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
