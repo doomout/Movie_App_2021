03026f145a9013bfd297bbd9f47c385c7513fd36
김현구 <doomout1@gmail.com>
2021-12-11 03:25:46 +0900

    id 추가

--- src/App.js
+++ src/App.js
@@ -2,35 +2,38 @@ import React from "react";
 
 //인자를 받는다
 function Food({ name, picture }) {
-  //console.log(fav); //로그찍기
-  //{ fav } = props;
   return (
       <div>
         <h2>I like {name}</h2>
-        <img src={picture} />
+        <img src={picture} alt={name} />
       </div>
     );
 }
 
 const foodILike = [
   {
+    id: 1,
     name: '김치',
     image: 'http://08food.com/web/product/big/201801/33_shop1_243196.jpg',
   },
   {
+    id: 2,
     name: '라면',
     image: 'https://health.chosun.com/site/data/img_dir/2020/09/07/2020090702900_0.jpg',
   },
   {
+    id: 3,
     name: '김밥',
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Gimbap_%28pixabay%29.jpg/220px-Gimbap_%28pixabay%29.jpg',
   },
 ];
 
+
+
 function App() {
   return (
     <div>
-      {foodILike.map(dish => (<Food name={dish.name} picture={dish.image}/>))}
+      {foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} />))}
     </div>
   );
 }
