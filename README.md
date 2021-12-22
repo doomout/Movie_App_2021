c48f5089bc6096cdffd8cd345725820afdd19812
doomout <winquake3@hanmail.net>
2021-12-23 01:41:42 +0900

    라우터 기능 추가 (react-router-dom 5 버전 사용)

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Movie App 2021
 
-리액트 영화앱 만들기 공부
+리액트 영화앱 만들기 공부 중
+8강을 진행 하기 위해서는 react-router-dom 설치시 5버전을 설치해야 한다.
+npm install react-router-dom@5 를 입력한다. 
--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import Home from './routes/Home';
 import './App.css';
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+//import Navigation from './components/Navigation';
+//import Detail from './routes/Detail';
 
+
+//Route를 사용하기 위해선 npm install react-router-dom@5 으로 5 버전을 써야 한다
+//6 버전은 <Routes></Routes>로 묶어줘야 하지만 내부가 보이지 않는다. 겨우 찾았네 ㅡ.ㅡ
 function App() {
-  return <Home />;
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
