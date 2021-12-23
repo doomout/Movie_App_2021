71a1645244cf939454540f58d538a8a1609a76a9
doomout <winquake3@hanmail.net>
2021-12-23 22:40:44 +0900

    Navigation 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { HashRouter, Route } from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
-//import Navigation from './components/Navigation';
+import Navigation from './components/Navigation';
 //import Detail from './routes/Detail';
 
 
@@ -12,6 +12,7 @@ import Home from './routes/Home';
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
     </HashRouter>
